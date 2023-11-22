cc8562a3b3a20a734d265d1c9853b66c31c4549f
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-11-22 22:15:00 +0100

    new code format + fixes

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,7 @@
 // responsive nav-bar
-const bars = document.querySelector(".bars");
-const navBar = document.querySelector(".nav-bar");
-const lines = document.querySelectorAll(".line");
+const bars = document.querySelector(".header__hamburger");
+const navBar = document.querySelector(".header__nav");
+const lines = document.querySelectorAll(".hamburger__line");
 
 bars.onclick = function () {
   navBar.classList.toggle("active");
