d15cd31b0cde7a3e0fcdd329ff7d4eac0f938ce0
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-11-23 22:05:56 +0100

    hero background implemented

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
 // responsive nav-bar
 const bars = document.querySelector(".header__hamburger");
 const navBar = document.querySelector(".header__nav");
+const header = document.querySelector(".header");
 const lines = document.querySelectorAll(".hamburger__line");
 
 bars.onclick = function () {
   navBar.classList.toggle("active");
+  header.classList.toggle("active");
   lines.forEach((line) => {
     line.classList.toggle("active");
   });
