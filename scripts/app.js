7824173f0f406b9ba0d633eacb1fc012d92f7232
nicolasleone1 <leoneklyver@gmail.com>
2023-11-30 16:49:39 -0300

    nav bg
    changes in nav-bg

--- scripts/app.js
+++ scripts/app.js
@@ -3,13 +3,30 @@ const bars = document.querySelector(".header__hamburger");
 const navBar = document.querySelector(".header__nav");
 const header = document.querySelector(".header");
 const lines = document.querySelectorAll(".hamburger__line");
+const hero = document.querySelector(".hero__section-bg");
+
+// Agrega una transición al fondo para cerrar el menú
+hero.style.transition = "opacity 0.5s ease-in-out";
 
 bars.onclick = function () {
   navBar.classList.toggle("active");
   header.classList.toggle("active");
+  hero.classList.toggle("active");
+
   lines.forEach((line) => {
     line.classList.toggle("active");
   });
-};
 
-// swiper
+  
+  if (navBar.classList.contains("active")) {
+    hero.style.transition = "opacity 0s";
+  } else {
+    hero.style.transition = "opacity 1.8s ease-in-out"; 
+  }
+
+  if (hero.classList.contains("active")) {
+    hero.style.opacity = "0";
+  } else {
+    hero.style.opacity = "1"; 
+  }
+};
