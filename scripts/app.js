38cdcc05bf5aae1373159d7cd53c86f2a674c354
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-12-02 13:13:24 +0100

    improved header functionality and compatibility

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,9 @@
 // responsive nav-bar
 const bars = document.querySelector(".header__hamburger");
-const navBar = document.querySelector(".header__nav");
+const navBar = document.querySelector(".header__nav-mobile");
 const header = document.querySelector(".header");
 const lines = document.querySelectorAll(".hamburger__line");
 const hero = document.querySelector(".hero__section-bg");
-
-// Agrega una transición al fondo para cerrar el menú
-hero.style.transition = "opacity 0.5s ease-in-out";
 
 bars.onclick = function () {
   navBar.classList.toggle("active");
@@ -17,16 +14,9 @@ bars.onclick = function () {
     line.classList.toggle("active");
   });
 
-  
-  if (navBar.classList.contains("active")) {
-    hero.style.transition = "opacity 0s";
-  } else {
-    hero.style.transition = "opacity 1.8s ease-in-out"; 
-  }
-
   if (hero.classList.contains("active")) {
     hero.style.opacity = "0";
   } else {
-    hero.style.opacity = "1"; 
+    hero.style.opacity = "1";
   }
 };
